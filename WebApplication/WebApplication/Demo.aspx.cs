442d528910b919c2fcfa63aa248d289b9262260e
Camilo Flórez <jcflorezv@unal.edu.co>
2019-05-30 03:00:08 -0500

    controls

---
+++ WebApplication/WebApplication/Demo.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WebApplication
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Response.Write(txtName.Text + "</br>");
+            Response.Write(lstLocation.SelectedItem.Text + "</br>");
+
+            lblName.Visible = false;
+            txtName.Visible = false;
+            lstLocation.Visible = false;
+            chkC.Visible = false;
+            chkJava.Visible = false;
+            chkPython.Visible = false;
+            rdMale.Visible = false;
+            rdFemale.Visible = false;
+            btnSubmit.Visible = false;
+        }
+    }
+}
